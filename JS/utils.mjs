041df0060dd4cef5a3e02824dc5e2fbdf4ab1247
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-01-31 20:23:40 -0700

    Weather Summary module finished with class

---
+++ JS/utils.mjs
@@ -0,0 +1,8 @@
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+// save data to local storage
+export function setLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
