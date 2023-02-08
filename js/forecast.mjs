8efe82e42182f3caa957408fd36f96f816f489a3
Ryan Schmidt <blueboard2014@aol.com>
2023-02-08 13:29:15 -0700

    everything but precipitation

--- js/forecast.mjs
+++ js/forecast.mjs
@@ -1,4 +1,4 @@
-import { sundayThruMondayInator } from "./utils.mjs";
+import { sundayThruMondayInator, mode, average } from "./utils.mjs";
 
 //A function to convert responses to json and checks if ok
 async function convertToJson(res) {
@@ -16,12 +16,22 @@ class Forecast {
         this.longitude = long;
         this.apiKey = "4491eb92629e7b5e0ac20b732e39129e";
         this.degreeType = deg;
+        // setting up some text for display in the forecast
+        this.degreeSymbol = "";
+        this.windSpeed = "";
+        if (this.degreeType == "metric"){
+            this.degreeSymbol = "C";
+            this.windSpeed = "kph";
+        } else if (this.degreeType == "imperial") {
+            this.degreeSymbol = "F";
+            this.windSpeed = "mph";
+        }   
     }
 
     init() {
         this.createURL();
         this.getForecast();
-        this.renderSummary();        
+        this.renderSummary();      
     }
 
     //create the url path for the specified city and degree type
@@ -38,29 +48,31 @@ class Forecast {
     
     // ***************************Start 3-hour Section*****************************************//
     
+    
+
     //A function that returns a string literal to populate the five-day-forecast container
     renderSummary(number) {
-        let degreeSymbol = "";
-        let windSpeed = "";
-        if (this.degreeType == "metric"){
-            degreeSymbol = "C";
-            windSpeed = "kph";
-        } else if (this.degreeType == "imperial") {
-            degreeSymbol = "F";
-            windSpeed = "mph";
-        } 
+        
         return `<h3 id="day-${number}"></h3>
-        <p>High:<span id="high-temp-${number}"></span>°${degreeSymbol}</p>
-        <p>Low:<span id="low-temp-${number}"></span>°${degreeSymbol}</p>
+        <p>High:<span id="high-temp-${number}"></span>°${this.degreeSymbol}</p>
+        <p>Low:<span id="low-temp-${number}"></span>°${this.degreeSymbol}</p>
         <img id="weatherImage-${number}" src="">
-        <p>Wind:<span id="windSpeed-${number}"></span> ${windSpeed}</p>
+        <p>Wind:<span id="windSpeed-${number}"></span> ${this.windSpeed}</p>
         <p><span id="weathDesc-${number}"></span></p>
         <button id="show-hide-button-${number}" class="hidden"></button>`
     }
+    
     //A function that dynamically renders the five-day-forecast container
     populateSummaries(dataObject) {
-        // console.log(dataObject);
+        console.log(dataObject);
         // creating variables for the 5-day forecast cards
+        let high_temp_max = -1000;
+        let low_temp_min = 1000;
+        let most_common_weather_obj_arrays = {"icon":[], "weather_desc":[]};
+        let most_common_weather = {"icon":"", "weather_desc":""};
+        let wind_speeds = [];
+        let average_wind_speed = 0;
+        let newDay = true;
         for(let i in dataObject.list) {
 
             //Use destructoring to enable readable code and pull specific properties from our object
@@ -71,9 +83,7 @@ class Forecast {
             const weathDesc = dataObject.list[i].weather[0].description;
             const dt = new Date(dataObject.list[i].dt * 1000);
             const dayOfWeek = sundayThruMondayInator(dt.getDay());
-            const timeOfDay = dt.toLocaleTimeString("en-US", {hour: '2-digit'})
-
-            // Calculating Averages to use in 5-day Forecast
+            const timeOfDay = dt.toLocaleTimeString("en-US", {hour: '2-digit'});
 
             
 
@@ -87,15 +97,47 @@ class Forecast {
                 document.getElementById(`five-day-forecast-${dayOfWeek}`).innerHTML = fiveDayHTML;
                 document.getElementById(`day-${dayOfWeek}`).textContent = `${dayOfWeek}`;
                 document.getElementById(`show-hide-button-${dayOfWeek}`).addEventListener("click", () => {for(let i in document.getElementsByClassName(`${dayOfWeek}`)){document.getElementsByClassName(`${dayOfWeek}`)[i].classList.toggle("hidden")}});
+                newDay = true;
             }
+
+            // Calculating highs, lows, and averages for 5-day
+            if(newDay){
+                // resetting variables for new day
+                high_temp_max = -1000;
+                low_temp_min = 1000;
+                most_common_weather_obj_arrays = {"icon":[], "weather_desc":[]};
+                most_common_weather = {"icon":"", "weather_desc":""};
+                wind_speeds = [];
+                average_wind_speed = 0;
+            }
+            // Low for day
+            if(low_temp_min > temp_min){
+                low_temp_min = temp_min;
+            }
+            // High for day
+            if(high_temp_max < temp_max){
+                high_temp_max = temp_max;
+            }
+            // Most common weather for day
+            most_common_weather_obj_arrays["icon"].push(weatherImage);
+            most_common_weather_obj_arrays["weather_desc"].push(weathDesc);
+            most_common_weather["icon"] = mode(most_common_weather_obj_arrays["icon"]);
+            most_common_weather["weather_desc"] = mode(most_common_weather_obj_arrays["weather_desc"]);
+
+            // average winds speeds for day
+            wind_speeds.push(speed);
+            average_wind_speed = average(wind_speeds);
+
+            // variable to manage calculating highs and lows for individual days, resets to true in the if statement that checks if the document already has a section element for the particular day roughly thirty lines above this as of right now.
+            newDay = false;
 
             //grab 5-day averages containers by their id's and populate them with results calculated with data from the API
             
-            document.getElementById(`high-temp-${dayOfWeek}`).textContent = `\u00A0\u00A0${Math.floor(temp_max)}`;
-            document.getElementById(`low-temp-${dayOfWeek}`).textContent = `\u00A0\u00A0${Math.floor(temp_min)}`;
-            document.getElementById(`weatherImage-${dayOfWeek}`).src = `http://openweathermap.org/img/wn/${weatherImage}@2x.png`
-            document.getElementById(`weathDesc-${dayOfWeek}`).textContent = `${weathDesc}`;
-            document.getElementById(`windSpeed-${dayOfWeek}`).textContent = `\u00A0\u00A0${Math.ceil(speed)}`;
+            document.getElementById(`high-temp-${dayOfWeek}`).textContent = `\u00A0\u00A0${Math.floor(high_temp_max)}`;
+            document.getElementById(`low-temp-${dayOfWeek}`).textContent = `\u00A0\u00A0${Math.floor(low_temp_min)}`;
+            document.getElementById(`weatherImage-${dayOfWeek}`).src = `http://openweathermap.org/img/wn/${most_common_weather["icon"]}@2x.png`;
+            document.getElementById(`weathDesc-${dayOfWeek}`).textContent = `${most_common_weather["weather_desc"]}`;
+            document.getElementById(`windSpeed-${dayOfWeek}`).textContent = `\u00A0\u00A0${Math.ceil(average_wind_speed)}`;
             document.getElementById(`show-hide-button-${dayOfWeek}`).innerHTML = `show/hide 3-hour forecast`;
             document.getElementById(`show-hide-button-${dayOfWeek}`).classList.remove("hidden");
             
@@ -113,8 +155,12 @@ class Forecast {
 
             //grab containers by their id's and populate them with data from the API
             document.getElementById(`day-${i}`).textContent = `${timeOfDay}`; // ${dayOfWeek.substring(0,3)} 
+            document.getElementById(`high-temp-${i}`).parentElement.style = "grid-row: 2/3; font-size: 2em; margin-left: 0;";
+            document.getElementById(`high-temp-${i}`).parentElement.innerHTML = `<span id="high-temp-${i}"></span>°${this.degreeSymbol}`;
             document.getElementById(`high-temp-${i}`).textContent = `\u00A0\u00A0${Math.floor(temp_max)}`;
-            document.getElementById(`low-temp-${i}`).textContent = `\u00A0\u00A0${Math.floor(temp_min)}`;
+            document.getElementById(`low-temp-${i}`).parentElement.parentElement.style = "grid-template-rows: auto auto;";
+            document.getElementById(`low-temp-${i}`).parentElement.innerHTML = "";
+            
             document.getElementById(`weatherImage-${i}`).src = `http://openweathermap.org/img/wn/${weatherImage}@2x.png`
             document.getElementById(`weathDesc-${i}`).textContent = `${weathDesc}`;
             document.getElementById(`windSpeed-${i}`).textContent = `\u00A0\u00A0${Math.ceil(speed)}`;
@@ -126,5 +172,5 @@ class Forecast {
 }
 
 
-const forecast_placeholder = new Forecast("51.5085", "-0.1257", "metric");
+const forecast_placeholder = new Forecast("41.2230", "-111.9738", "imperial");
 forecast_placeholder.init();
--- js/utils.mjs
+++ js/utils.mjs
@@ -20,3 +20,43 @@ export function sundayThruMondayInator(dayOfWeekNumber){
     ];
     return daysInWeek[dayOfWeekNumber];
 }
+export function mode(array) {
+    //checking to see if the array is empty first to not get errors from this function
+    if (array.length === 0) {
+        return null;
+    }
+
+    let frequency = {};
+    let biggestFrequency = 0;
+    let mode;
+    
+    for (let i = 0; i < array.length; i++) {
+        // creating a new key value pair in the object frequency for each item in the list and using the object itself as the key and the count as the value, the count (or value) increments by 1 each time the new key is found in the array
+        let item = array[i];
+        if (frequency[item]) {
+            frequency[item]++;
+        } else {
+            frequency[item] = 1;
+        }
+        // generic mode stuff, if the new number is bigger, use it instead of the old one. This particular model favors 1. Whichever number is bigger, then 2. Whichever number came first.
+        if (frequency[item] > biggestFrequency) {
+            biggestFrequency = frequency[item];
+            mode = item;
+        }
+    }
+    
+    return mode;
+}
+export function average(array) {
+    // double checking the array is not empty to avoid errors
+    if (array.length === 0) {
+        return null;
+    }
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+    return sum / array.length;
+}
